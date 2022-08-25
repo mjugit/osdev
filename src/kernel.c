5d5b5ad1ffb9b05afa9950ca76a6493b5ba48391
Maximilian Jung <maximilian.jung@posteo.de>
2022-08-25 20:02:59 +0000

    #2 Refactor kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -2,23 +2,27 @@
 #include <stddef.h>
 
 #if defined(__linux__)
-#error "This program needs to be build with a cross compiler"
+#  error "This program needs to be build with a cross compiler"
 #endif
 
 #if !defined(__i386__)
-#error "You have to use a ix86-elf compiler!"
+#  error "You have to use a ix86-elf compiler!"
 #endif
 
 #define SCREEN_BUFFER 0xB8000
 #define SCREEN_COLS 80
 #define SCREEN_ROWS 25
 
+void scr_init(void);
+void scr_write(const char *text);
+void kernel_main(void);
+
 enum color {
   COLOR_BLACK = 0,
   COLOR_WHITE = 15
 };
 
-static inline uint8_t mk_color_pair(enum color fg, enum color bg) {
+static inline uint8_t scr_color(enum color fg, enum color bg) {
   return fg | bg << 4;
 }
 
@@ -26,8 +30,8 @@ static inline uint16_t char_entry(unsigned char uc, uint8_t color) {
   return (uint16_t) uc | (uint16_t) color << 8;
 }
 
-void screen_init(void) {
-  uint8_t color = mk_color_pair(COLOR_BLACK, COLOR_WHITE);
+void scr_init(void) {
+  uint8_t color = scr_color(COLOR_BLACK, COLOR_WHITE);
 
   for (size_t crow = 0; crow < SCREEN_ROWS; crow++) {
     for (size_t ccol = 0; ccol < SCREEN_COLS; ccol++) {
@@ -37,8 +41,8 @@ void screen_init(void) {
   }
 }
 
-void write_string(const char *text) {
-  uint8_t color = mk_color_pair(COLOR_BLACK, COLOR_WHITE);
+void scr_write(const char *text) {
+  uint8_t color = scr_color(COLOR_BLACK, COLOR_WHITE);
   unsigned int text_len = 0;
 
   for (; text[text_len]; text_len++);
@@ -47,6 +51,6 @@ void write_string(const char *text) {
 }
 
 void kernel_main(void) {
-  screen_init();
-  write_string("Hello, world!\0");
+  scr_init();
+  scr_write("Hello, world!\0");
 }
