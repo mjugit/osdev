380ad7c29203a0379cb584ad6d353ecf9094f214
Maximilian Jung <maximilian.jung@posteo.de>
2022-08-27 02:45:44 +0000

    #3 Add function to fill area

--- src/kernel.c
+++ src/kernel.c
@@ -44,7 +44,7 @@ typedef uint16_t ch_t;
 typedef uint8_t ch_attr_t;
 
 
-static ch_t *_screen_buffer;
+static ch_t _screen_buffer[SCREEN_COLS * SCREEN_ROWS];
 static ch_attr_t _theme;
 
 
@@ -52,8 +52,10 @@ void memcpy(void *dst, void *src, size_t count);
 void memset(void *dst, void *src, size_t size, size_t count);
 void tui_init(size_t cols, size_t rows);
 void tui_write_at(size_t col, size_t row, char *text);
+void tui_refresh(void);
 
 void kernel_main(void);
+
 
 void memcpy(void *dst, void *src, size_t count) {
   for (size_t offset = 0; offset < count; offset++)
@@ -67,13 +69,13 @@ void memset(void *dst, void *src, size_t size, size_t count) {
 
 
 void tui_init(size_t cols, size_t rows) {
-  _theme = colorpair(VGA_CYAN, VGA_BLUE);
+  _theme = colorpair(VGA_BLACK, VGA_LIGHT_GREY);
   ch_t background = ch(' ', _theme);
 
   memset(_screen_buffer, &background, sizeof(ch_t), cols * rows);
 }
 
-void tui_write_at(size_t col, size_t row, char *text) {
+void tui_printat(size_t col, size_t row, char *text) {
   size_t pos = cursor(col, row);
   
   for (size_t len = 0; text[len]; (len++), (pos++)) {
@@ -83,12 +85,23 @@ void tui_write_at(size_t col, size_t row, char *text) {
   }
 }
 
+void tui_drawrect(size_t width, size_t height, size_t posh, size_t posv, ch_t bg) {
+  for (size_t row = posv; row < posv + height; row++) {
+    for (size_t col = posh; col < width + posh; col++) {
+      memset(&_screen_buffer[cursor(col, row)], &bg, sizeof(ch_t), 1);
+    }
+  }
+}
+
+void tui_refresh(void) {
+  memcpy((void*)SCREEN_BUFFER, _screen_buffer, sizeof(_screen_buffer));
+}
 
 
 void kernel_main(void) {
-  _screen_buffer = (ch_t*)SCREEN_BUFFER;
   tui_init(SCREEN_COLS, SCREEN_ROWS);
-  tui_write_at(5, 0, "X (5, 0)\0");
-  tui_write_at(2, 3, "X (2, 3)\0");
-  tui_write_at(8, 10, "X (8, 10)\0");
+  tui_drawrect(4, SCREEN_ROWS, 0, 0, ch(0xb1, colorpair(VGA_BLACK, VGA_LIGHT_GREY)));
+  tui_drawrect(1, SCREEN_ROWS, 4, 0, ch(0xdd, colorpair(VGA_BLACK, VGA_LIGHT_GREY)));
+  tui_printat(5, 3, "Lorem ipsum dolor sit amet\0");
+  tui_refresh();
 }
