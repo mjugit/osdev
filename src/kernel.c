1452bdb61bdcfdb4bc2a207f648eaf03ca9770a3
Maximilian Jung <maximilian.jung@posteo.de>
2022-08-31 17:20:51 +0000

    #3 Add font bitmap processing prototype

--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#include "console-font.h"
+
 #if defined(__linux__)
 #  error "This program needs to be build with a cross compiler"
 #endif
@@ -9,53 +11,12 @@
 #  error "You have to use a ix86-elf compiler!"
 #endif
 
-#define SCREEN_BUFFER 0xB8000
-#define SCREEN_COLS 80
-#define SCREEN_ROWS 25
-
-// MACROS
-#define colorpair(fg,bg) ((fg) | ((bg) << 4))
-// Inline 
-#define ch(ch, attr) ((ch) | ((attr) << 8))
-// Get the linear offset realive to the start of the video buffer
-#define cursor(col, row) ((col) + ((row) * (SCREEN_COLS)))
-#define strlen(src, len) (for (len = 0; src[len]; len++)) 
-
-typedef enum {
-  VGA_BLACK	    = 0,
-  VGA_BLUE	    = 1,
-  VGA_GREEN	    = 2,
-  VGA_CYAN	    = 3,
-  VGA_RED	    = 4,
-  VGA_MAGENTA	    = 5,
-  VGA_BROWN	    = 6,
-  VGA_LIGHT_GREY    = 7,
-  VGA_DARK_GREY	    = 8,
-  VGA_LIGHT_BLUE    = 9,
-  VGA_LIGHT_GREEN   = 10,
-  VGA_LIGHT_CYAN    = 11,
-  VGA_LIGHT_RED	    = 12,
-  VGA_LIGHT_MAGENTA = 13,
-  VGA_LIGHT_BROWN   = 14,
-  VGA_WHITE	    = 15
-} color;
-
-typedef uint16_t ch_t;
-typedef uint8_t ch_attr_t;
-
-
-static ch_t _screen_buffer[SCREEN_COLS * SCREEN_ROWS];
-static ch_attr_t _theme;
-
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
 
 void memcpy(void *dst, void *src, size_t count);
 void memset(void *dst, void *src, size_t size, size_t count);
-void tui_init(size_t cols, size_t rows);
-void tui_write_at(size_t col, size_t row, char *text);
-void tui_refresh(void);
-
 void kernel_main(void);
-
 
 void memcpy(void *dst, void *src, size_t count) {
   for (size_t offset = 0; offset < count; offset++)
@@ -67,41 +28,33 @@ void memset(void *dst, void *src, size_t size, size_t count) {
     memcpy(dst + offset * size, src, size);
 }
 
-
-void tui_init(size_t cols, size_t rows) {
-  _theme = colorpair(VGA_BLACK, VGA_LIGHT_GREY);
-  ch_t background = ch(' ', _theme);
-
-  memset(_screen_buffer, &background, sizeof(ch_t), cols * rows);
+void set_pixel(int pos_x, int pos_y, uint32_t color) {
+  void* location = (void*)0xA0000 + (SCREEN_WIDTH * pos_y * sizeof(color)) + (pos_x * sizeof(color));
+  memset(location, &color, 4, 1);
 }
 
-void tui_printat(size_t col, size_t row, char *text) {
-  size_t pos = cursor(col, row);
+void putc(int pos_x, int pos_y, char ch, uint32_t color) {
+  const int FONT_HEIGHT = 16;
+  const int FONT_WIDTH = 12;
+  const int FONT_BITS_PER_ROW = 16;
+  uint16_t char_bitmap[FONT_HEIGHT];
   
-  for (size_t len = 0; text[len]; (len++), (pos++)) {
-    ch_t next_char = ch(text[len], _theme);
-    memset(_screen_buffer + pos, &next_char, sizeof(ch_t), 1);
-    pos++;
-  }
-}
+  memcpy(&char_bitmap, FONT_12X16 + ch * 16, sizeof(uint16_t) * 16);
+  
+  for (int ch_row = 0; ch_row < FONT_HEIGHT; ch_row++) {
+    uint16_t row_bitmap = char_bitmap[ch_row] >> FONT_BITS_PER_ROW - FONT_WIDTH;
 
-void tui_drawrect(size_t width, size_t height, size_t posh, size_t posv, ch_t bg) {
-  for (size_t row = posv; row < posv + height; row++) {
-    for (size_t col = posh; col < width + posh; col++) {
-      memset(&_screen_buffer[cursor(col, row)], &bg, sizeof(ch_t), 1);
+    for (int ch_col = 11; ch_col >= 0; ch_col--) {
+      if (row_bitmap & 1)
+	set_pixel(pos_x + ch_col, pos_y + ch_row, color);
+      row_bitmap = row_bitmap >> 1;
     }
   }
 }
 
-void tui_refresh(void) {
-  memcpy((void*)SCREEN_BUFFER, _screen_buffer, sizeof(_screen_buffer));
-}
-
-
 void kernel_main(void) {
-  tui_init(SCREEN_COLS, SCREEN_ROWS);
-  tui_drawrect(4, SCREEN_ROWS, 0, 0, ch(0xb1, colorpair(VGA_BLACK, VGA_LIGHT_GREY)));
-  tui_drawrect(1, SCREEN_ROWS, 4, 0, ch(0xdd, colorpair(VGA_BLACK, VGA_LIGHT_GREY)));
-  tui_printat(5, 3, "Lorem ipsum dolor sit amet\0");
-  tui_refresh();
+  char *str = "This is just a simple test.\0";
+
+  for (int i = 0; str[i]; i++)
+    putc(i * 12, 0, str[i], 0xffffffff);
 }
